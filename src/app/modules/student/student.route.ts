8490b2527a10fa27fbd40635bb968dfeefa91212
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-08 22:55:21 +0600

    delete student schema

--- src/app/modules/student/student.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { studentController } from './student.controller'
-
-import { upadateZStudentSchema } from './student.validation'
-
-const router = express.Router()
-
-export const studentRoutes = router
