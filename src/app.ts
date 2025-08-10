e2a6271c85c699f20e29b84ba3fa6d731026cdd2
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-10 22:50:18 +0600

    set cors errors

--- src/app.ts
+++ src/app.ts
@@ -8,20 +8,10 @@ import cookieParser from 'cookie-parser'
 // ! Parser
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = [
-  'https://quiz-test-rho.vercel.app',
-  'http://localhost:5173',
-]
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
+    origin: 'https://quiz-test-rho.vercel.app',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -29,7 +19,13 @@ app.use(
 )
 
 // Preflight route
-app.options('*', cors())
+app.options(
+  '*',
+  cors({
+    origin: 'https://quiz-test-rho.vercel.app',
+    credentials: true,
+  }),
+)
 app.get('/', (req: Request, res: Response) => {
   res.send('Test-School server is running')
 })
